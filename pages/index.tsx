867d0f3943f540d771022e6c5efe3a16df7fe7ea
Rehan Tariq <rehantariq1858@gmail.com>
2024-09-03 01:41:46 +0500

    BOOK DOWNLOAD

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import Footer1 from "../components/footer1";
 import Header1 from "../components/header1";
 import WhySection from "../components/Whysection";
 import TypesSection from "../components/typesSeptic";
+import PdfDownload from "../components/pdfdownload";
 
 const WillaimsSeptic: NextPage = () => {
   return (
@@ -19,6 +20,7 @@ const WillaimsSeptic: NextPage = () => {
       <WhySection />
       <TypesSection />
       {/* <Williams /> */}
+      <PdfDownload />
       <section className="self-stretch flex flex-row items-start justify-center pt-0 px-5 pb-2.5 text-left text-45xl text-black font-body-3">
         <h1 className="m-0 h-[83px] relative text-inherit font-bold font-[inherit] inline-block mq450:text-19xl mq1050:text-32xl"
         data-scroll-to='gallery'
