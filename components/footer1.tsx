f00566c3fb95ee9cf061d8acb5d1eec7a9bf2178
Rehan Tariq <rehantariq1858@gmail.com>
2024-09-04 17:49:35 +0500

    contact update

--- components/footer1.tsx
+++ components/footer1.tsx
@@ -67,7 +67,8 @@ const Footer1: NextPage<Footer1Type> = ({
   }, []);
 
   const onContactUsTextClick = useCallback(() => {
-    router.push("https://getjobber.com/");
+    router.push(      "https://clienthub.getjobber.com/client_hubs/c6ab68fa-74c2-4d46-9066-ff2210b88264/public/work_request/new?source=social_media"
+);
   }, [router]);
 
   const onBlogTextClick = useCallback(() => {
